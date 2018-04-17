092d111214d1d285d99ed90dc40d48d1eac9b743
Meredith Rawls <mrawls@uw.edu>
2018-04-17 11:16:46 -0700

    Add Hours to Minutes and fix indentations

--- conversion.py
+++ conversion.py
@@ -1,10 +1,13 @@
 # My Conversion Tools
 
 def dollars2cents(dollars):
-    cents = dollars * 100
-    return cents
+   cents = dollars * 100
+   return cents
 
 def gallons2liters(gallons):
-    liters = gallons * 3.78541
-    return liters
+   liters = gallons * 3.78541
+   return liters
 
+def hours2minutes(hours):
+   minutes = hours * 60
+   return hours
